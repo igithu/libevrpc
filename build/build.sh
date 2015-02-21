c10c97066cd5bd43553f5383780b0ab4d2ee4103
igithu <asy5178@163.com>
2015-02-21 21:50:27 +0800

    add build file

---
+++ build/build.sh
@@ -0,0 +1,87 @@
+#!/bin/sh
+#!/bin/bash
+
+#build the data_server
+#must have the bootstrap.sh file or be failed for sure
+#must install automake
+
+BUILD_ROOT=$PWD
+START_FILE=$BUILD_ROOT/libevrpc.sh
+SERVER_PATH=$BUILD_ROOT/libevrpc
+SERVER_BIN_PATH=$SERVER_PATH/bin
+CONF_PATH=$BUILD_ROOT/../../conf
+CONF_FILE=$CONF_PATH/ds.ini 
+
+echo $BUILD_ROOT
+
+# check the bootstrap exist.
+if [ ! -f $BUILD_ROOT/bootstrap.sh ]
+then
+    echo "the bootstrap.sh does not exist!"
+    exit -1
+fi
+
+# clean the .o file in order to make the proj
+if [ -d $SERVER_PATH ] 
+then
+    rm -rf $SERVER_PATH
+fi
+make distclean && ./bootstrap.sh clean
+./bootstrap.sh
+
+# check the configure file 
+if [ ! -f $BUILD_ROOT/configure ]
+then
+    echo "the configure does not exist!"
+    exit -1
+fi
+
+# make the project
+./configure && make
+if [ $? -ne 0 ]
+then
+    echo "make the project failed!"
+    exit -1
+fi
+
+
+
+#start to collect the project file 
+
+## make sure the config file exist
+#if [ ! -f $CONF_FILE ]
+#then
+#    echo "the config file $CONF_FILE does not exist"
+#    exit -1
+#fi
+
+if [ ! -f $START_FILE ]
+then
+    echo "the $START_FILE file does not exist"
+    exit -1
+fi
+
+
+# make sure the path SERVER_BIN_PATH exist
+
+if [ ! -d $SERVER_BIN_PATH ]
+then
+    mkdir -p $SERVER_BIN_PATH
+fi 
+
+
+mv rpc_client rpc_server $SERVER_BIN_PATH && \
+#cp -r $CONF_PATH $SERVER_PATH && \
+#cp $START_FILE $SERVER_BIN_PATH
+
+if [ $? -ne 0 ]
+then
+    echo "build the clint bin failed"
+    exit -1
+fi
+
+echo "build the name server successfully!!"
+
+make distclean && ./bootstrap.sh clean && rm COPYING
+
+
