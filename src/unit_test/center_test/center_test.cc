e626ba38f6c7519cb33e08538b04abd21b0806a1
alisen <asy5178@163.com>
2016-12-23 00:22:57 +0800

    add unit center test

---
+++ src/unit_test/center_test/center_test.cc
@@ -0,0 +1,37 @@
+/***************************************************************************
+ *
+ * Copyright (c) 2016 aishuyu, Inc. All Rights Reserved
+ *
+ **************************************************************************/
+
+
+
+/**
+ * @file center_test.cc
+ * @author aishuyu(asy5178@163.com)
+ * @date 2016/12/22 21:57:08
+ * @brief
+ *
+ **/
+
+#include "rpc_center.h"
+
+int main() {
+    RpcCenter& rc = RpcCenter::GetInstance("conf/rpc_center.ini");
+    rc.StartCenter();
+    rc.WaitCenter();
+    rc.StopCenter();
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+/* vim: set expandtab ts=4 sw=4 sts=4 tw=100: */
