a8e10fc9ffba0bee7a4bb95f0760226441562b01
alisen <asy5178@163.com>
2017-03-16 00:01:24 +0800

    roll back

--- src/rpc_server/center_cluster_heartbeat.cpp
+++ src/rpc_server/center_cluster_heartbeat.cpp
@@ -186,7 +186,7 @@ void CenterClusterHeartbeat::Run() {
         RpcClusterServer rcs_proto;
         rcs_proto.set_cluster_action(CLUSTER_PING);
         rcs_proto.set_cluster_server_addr(GetLocalAddress());
-        // rcs_proto.set_load(s_info.loads[0]);
+        rcs_proto.set_load(s_info.loads[0]);
 
         string rcs_str;
         if (!rcs_proto.SerializeToString(&rcs_str)) {
